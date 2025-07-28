4de9c838ccb4b7c86a7250d2e07f86e275fa130e
Mohamed Bouallegue <mohamed.bouallegue@flouci.com>
2025-07-28 17:24:51 +0100

    revert adding password

--- utils/backend_client.py
+++ utils/backend_client.py
@@ -228,7 +228,6 @@ class FlouciBackendClient:
     @handle_exceptions
     def refund_pos_transaction(
         id_terminal,
-        password,
         serial_number,
         reason,
         merchant_id,
@@ -239,7 +238,6 @@ class FlouciBackendClient:
             "id_terminal": id_terminal,
             "serial_number": serial_number,
             "reason": reason,
-            "password": password,
             "merchant_id": merchant_id,
             "developer_tracking_id": developer_tracking_id,
             "transaction_id": flouci_transaction_id,
